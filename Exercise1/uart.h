00ff7422ded018b1cabc3b579bf75b8eefbe34b2
salmakishk7 <145611722+salmakishk7@users.noreply.github.com>
2024-01-15 00:01:15 +0200

    Add files via upload

---
+++ Exercise1/uart.h
@@ -0,0 +1,41 @@
+ /******************************************************************************
+ *
+ * Module: UART
+ *
+ * File Name: uart.h
+ *
+ * Description: Header file for the UART AVR driver
+ *
+ * Author: Mohamed Tarek
+ *
+ *******************************************************************************/
+
+#ifndef UART_H_
+#define UART_H_
+
+#include "micro_config.h"
+#include "std_types.h"
+#include "common_macros.h"
+
+/*******************************************************************************
+ *                      Preprocessor Macros                                    *
+ *******************************************************************************/
+#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 8UL))) - 1)
+
+/* UART Driver Baud Rate */
+#define USART_BAUDRATE 9200
+
+/*******************************************************************************
+ *                      Functions Prototypes                                   *
+ *******************************************************************************/
+void UART_init(void);
+
+void UART_sendByte(const uint8 data);
+
+uint8 UART_recieveByte(void);
+
+void UART_sendString(const uint8 *Str);
+
+void UART_receiveString(uint8 *Str); // Receive until #
+
+#endif /* UART_H_ */
